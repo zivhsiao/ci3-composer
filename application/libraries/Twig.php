8aba9a995a447a255533cb5658e6ee682e436e8a
Patrick <jenfu.developer@gmail.com>
2021-02-19 19:35:31 +0800

    first commit

---
+++ application/libraries/Twig.php
@@ -0,0 +1,31 @@
+<?php
+// libraries/Twig.php
+ 
+class Twig
+{
+    private $ci;
+    private $twig;
+ 
+    public function __construct()
+    {
+        // CodeIgniterのインスタンスを取得
+        $this->ci =& get_instance();
+ 
+        // Twigをnew
+        $loader = new Twig_Loader_Filesystem(VIEWPATH);
+        $this->twig = new Twig_Environment($loader);
+ 
+        // CodeIgniterのhelper関数をTwigに追加することもできる
+        $this->twig->addGlobal('baseUrl', base_url());
+        $this->twig->addFunction(new Twig\TwigFunction('setValue', 'set_value'));
+        $this->twig->addFunction(new Twig\TwigFunction('formError', 'form_error'));
+    }
+ 
+    // CodeIgniterのoutputにTwigのrenderを渡す
+    public function out($template, $data=[])
+    {
+        $this->ci->output->set_output(
+            $this->twig->render($template, $data)
+        );
+    }
+}
